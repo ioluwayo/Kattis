ef4cd7823626299ce778fd9e40c71a932e03da37
Ibukun <ioluwayo@ryerson.ca>
2016-12-20 00:48:26 -0500

    Solved day dreaming stockbroker

---
+++ daydreaming stockbrocker/solution.py
@@ -0,0 +1,29 @@
+infile = open("tests/1.in")
+numberOfDays = int(infile.readline().strip())
+availableMoney = 100
+sharesOwned = 0
+maxShares = 100000
+if numberOfDays == 1:
+    print availableMoney
+    exit()
+todaysPrice = int(infile.readline().strip())
+for i in xrange(numberOfDays-1):
+    tomorrowsPrice = int(infile.readline().strip())
+    if tomorrowsPrice > todaysPrice:
+        if sharesOwned<maxShares and availableMoney:
+            #buy some more
+            sharesOwned+=availableMoney/todaysPrice
+            availableMoney-=(availableMoney/todaysPrice)*todaysPrice
+            if sharesOwned>maxShares:
+                excess = sharesOwned-maxShares
+                availableMoney+=excess*todaysPrice
+                sharesOwned-=excess
+    elif tomorrowsPrice < todaysPrice:
+        # sell everything
+        availableMoney+=todaysPrice*sharesOwned
+        sharesOwned = 0
+    todaysPrice=tomorrowsPrice
+if sharesOwned:
+    availableMoney+=todaysPrice*sharesOwned
+    sharesOwned = 0
+print availableMoney
