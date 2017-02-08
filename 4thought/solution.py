a8d3b8a8cfe1b65cc5708d4c76d76cddcf9fb45e
Ibukun Oluwayomi <ioluwayo@Ibukuns-MacBook-Pro.local>
2017-02-07 23:50:54 -0500

    Solved 4thought

--- 4thought/solution.py
+++ 4thought/solution.py
@@ -1,17 +1,17 @@
-#unsolved
-from itertools import combinations_with_replacement
+#solved
+from itertools import product
 def do4thought(n):
-    operations = ["+","-","*","/"]
-    for i in combinations_with_replacement(operations,3):
-        fours = [int(4),int(4),int(4),int(4)]
+    operations = ["+","-","/","*"]
+    for i in product(operations,repeat = 3):
+        fours = [4, 4, 4, 4]
         fours.insert(1,i[0])
         fours.insert(3,i[1])
         fours.insert(5,i[2])
         fours =  " ".join(str(x) for x in fours)
         if eval(fours) == n:
                 return fours + " =" + " "+str(n)
-    for i in combinations_with_replacement(reversed(operations),3):
-        fours = [int(4), int(4), int(4), int(4)]
+    for i in product(reversed(operations),repeat = 3):
+        fours = [4, 4, 4, 4]
         fours.insert(1, i[0])
         fours.insert(3, i[1])
         fours.insert(5, i[2])
@@ -23,5 +23,4 @@ infile = open("tests/4thought.in")
 tests = int(infile.readline().strip())
 for i in xrange(tests):
     n = int(infile.readline().strip())
-#print map (lambda x:x, (i for i in permutations([6,8,9])))
     print do4thought(n)
